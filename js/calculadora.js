3d5aad28520183da1725adcc8c5c9fd5bf6bee5f
ianfa2201 <ianf.blancol@konradlorenz.edu.co>
2021-05-28 08:18:32 -0500

    Se agregan los archivos para el proyecto calculadora

---
+++ js/calculadora.js
@@ -0,0 +1,27 @@
+var a = document.getElementById("numero1");
+var b = document.getElementById("numero2");
+
+document.getElementById("suma").click(
+    function(a, b) {
+        var r = a + b;
+        return r;
+    }
+);
+
+document.getElementById("suma").click(
+    function(a, b) {
+
+    }
+);
+
+document.getElementById("suma").click(
+    function(a, b) {
+
+    }
+);
+
+document.getElementById("suma").click(
+    function(a, b) {
+
+    }
+);
