f8aa7c44723f77a23f1968f0762907661b0dc2b5
ianfa2201 <ianf.blancol@konradlorenz.edu.co>
2021-05-29 08:53:39 -0500

    Se terminó el proyecto de la calculadora, V.1.0

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,37 +1,63 @@
+function sumar() {
+    var a = document.getElementById("numero1").value;
+    var b = document.getElementById("numero2").value;       
+    var r = parseFloat(a) + parseFloat(b);
+    if(a=="" && b==""){
+        document.getElementById("result").innerHTML="Debe llenar los dos campos";
+    }else if (a=="") {
+        document.getElementById("result").innerHTML="Debe llenar el campo a";
+    } else if(b==""){
+        document.getElementById("result").innerHTML="Debe llenar el campo b";
+    }else
+    document.getElementById("result").innerHTML= r;
+}
 
-    function sumar() {
-        var a = document.getElementById("numero1").value;
-        var b = document.getElementById("numero2").value;
-        var r = parseInt(a) + parseInt(b);
+
+
+function restar() {
+    var a = document.getElementById("numero1").value;
+    var b = document.getElementById("numero2").value;
+    var r = a - b;
+    if(a=="" && b==""){
+        document.getElementById("result").innerHTML="Debe llenar los dos campos";
+    }else if (a=="") {
+        document.getElementById("result").innerHTML="Debe llenar el campo a";
+    } else if(b==""){
+        document.getElementById("result").innerHTML="Debe llenar el campo b";
+    }else
+    document.getElementById("result").innerHTML= r;
+}
+
+
+function multiplicar() {
+    var a = document.getElementById("numero1").value;
+    var b = document.getElementById("numero2").value;
+    var r = a * b;
+    if(a=="" && b==""){
+        document.getElementById("result").innerHTML="Debe llenar los dos campos";
+    }else if (a=="") {
+        document.getElementById("result").innerHTML="Debe llenar el campo a";
+    } else if(b==""){
+        document.getElementById("result").innerHTML="Debe llenar el campo b";
+    }else
+    document.getElementById("result").innerHTML= r;
+}
+
+
+
+function dividir() {
+    var a = document.getElementById("numero1").value;
+    var b = document.getElementById("numero2").value;
+    if(a=="" && b==""){
+        document.getElementById("result").innerHTML="Debe llenar los dos campos";
+    }else if (a=="") {
+        document.getElementById("result").innerHTML="Debe llenar el campo a";
+    } else if(b==""){
+        document.getElementById("result").innerHTML="Debe llenar el campo b";
+    }else if(b == 0){
+        document.getElementById("result").innerHTML= "No se puede dividir por cero";
+    } else {
+        var r = a / b;
         document.getElementById("result").innerHTML= r;
     }
-
-
-
-    function restar(a, b) {
-        var a = document.getElementById("numero1").value;
-        var b = document.getElementById("numero2").value;
-        var r = a - b;
-        document.getElementById("result").innerHTML= r;
-    }
-
-
-    function multiplicar(a, b) {
-        var a = document.getElementById("numero1").value;
-        var b = document.getElementById("numero2").value;
-        var r = a * b;
-        document.getElementById("result").innerHTML= r;
-    }
-
-
-
-    function dividir(a, b) {
-        var a = document.getElementById("numero1").value;
-        var b = document.getElementById("numero2").value;
-        if(b == 0){
-            document.getElementById("result").innerHTML= "No se puede dividir por cero";
-        } else {
-            var r = a / b;
-            document.getElementById("result").innerHTML= r;
-        }
-    }
+}
